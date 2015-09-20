e0c8f40c0c87fbb1a950ecbb78e1bee1d8115631
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-09-20 20:04:47 +0000

    1

--- task_01.py
+++ task_01.py
@@ -6,7 +6,7 @@
 import decimal
 
 
-ABSOLUTE_DIFFERENCE = decimal(271.15)
+ABSOLUTE_DIFFERENCE = decimal.Decimal('271.15')
 
 
 def celsius_to_kelvin(degrees):
@@ -17,13 +17,13 @@ def celsius_to_kelvin(degrees):
 
 def fahrenheit_to_celsius(degrees):
     """A function that convers fahrenheit to celsius."""
-    degrees = (degrees - 32) * decimal.Decimal(5) / 9
+    degrees = (degrees - 32) * decimal.Decimal('5') / 9
     return degrees.quantize(decimal.Decimal('0.000'))
 
 
 def fahrenheit_to_kelvin(degrees):
     """converting a Fahrenheit temperature to Kelvin
     """
-    degrees = (degrees - 32) * decimal.Decimal(5)/ 9
+    degrees = (degrees - 32) * decimal.Decimal('5')/ 9
     degrees = ABSOLUTE_DIFFERENCE + degrees
     return degrees.quantize(decimal.Decimal('0.00'))
