0c06259c38fa3dbcebf8db2b2fb9009a52558243
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-12-17 01:32:40 +0000

    re-done

--- task_01.py
+++ task_01.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task 1"""
+"""Degree Conversions"""
 
 
 import decimal
@@ -9,26 +9,62 @@ import decimal
 ABSOLUTE_DIFFERENCE = decimal.Decimal('273.15')
 
 
-def celsius_to_kelvin(degrees):
-    """converting a Celsius temperature to Kelvin
-    when degrees is passed a number it's added by ABSOLUTE_DIFFERENCE"""
-    degrees = ABSOLUTE_DIFFERENCE + degrees
-    return degrees
-
-
 def fahrenheit_to_celsius(degrees):
-    """converting fahrenheit to celsius.
-    degrees passed to Decimal. When a number is given, it's passed to
-    the equation in decimal form"""
-    degrees = (decimal.Decimal(degrees) - 32) * 5 / 9
-    return degrees
+    """This function converts a Celsius temperature to Kelvin.
+
+    Args:
+        degrees (int): the degree number to be converted to celsius.
+
+    Returns:
+        a degree in celsius
+
+    Examples:
+        >>> fahrenheit_to_celsius(212)
+        Decimal('100')
+
+        >>> fahrenheit_to_celsius(190)
+        Decimal('87.77777777777777777777777778')
+    """
+    celsius = (decimal.Decimal(degrees) - 32) * 5 / 9
+    return celsius
+
+
+def celsius_to_kelvin(degrees):
+    """This function converts a Celsius temperature to Kelvin.
+
+    Args:
+        degrees (int): the degree number to be converted to Kelvin.
+
+    Returns:
+        converted degree in Kelvin
+
+    Examples:
+        >>> fahrenheit_to_kelvin(100)
+        Decimal('373.15')
+
+
+        >>> fahrenheit_to_celsius(200)
+        Decimal('472.15')
+    """
+    kelvin = decimal.Decimal(ABSOLUTE_DIFFERENCE) + degrees
+    return kelvin
 
 
 def fahrenheit_to_kelvin(degrees):
-    """converting a Fahrenheit temperature to Kelvin
-    when degrees is passed a number, it's added by ABSOLUTE_DIFFERENCE,
-    and then the answer is passed to degrees = (decimal.Decimal(degrees)
-    - 32) * 5 / 9, calculation happens and then final answer is returned"""
-    degrees = (decimal.Decimal(degrees) - 32) * 5 / 9
-    degrees = ABSOLUTE_DIFFERENCE + degrees
-    return degrees
+    """Function converts a Fahrenheit temperature to Kelvin.
+
+    Args:
+        degrees(int): degrees to be converted to kelvin.
+
+    Returns:
+        degrees in kelvin.
+
+    Examples:
+        >>> fahrenheit_to_kelvin(212)
+        Decimal('373.15')
+
+        >>> fahrenheit_to_kelvin(41)
+        Decimal('230.69')
+    """
+    kelvin = celsius_to_kelvin(fahrenheit_to_celsius(degrees))
+    return kelvin
